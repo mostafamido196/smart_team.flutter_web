78e465d0e9adfdbce1123492f17f4382e2abb116
Mostafa <mostafamido196@gmail.com>
2025-02-05 04:06:23 +0200

    Built web
    Branch: main
    Commit: 00944dbbdbfcad2b7fa2b61c7084fcaec10c831c
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "514f84e5321c6ee28c43d37a957c66a8
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "8b08ba48667e863f7173a27aec2172cf",
+"flutter_bootstrap.js": "ff70a6b11bcb4c9cd2842fe0ce115884",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
